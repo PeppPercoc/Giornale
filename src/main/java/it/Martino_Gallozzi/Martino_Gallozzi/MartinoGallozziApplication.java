115b430a9e8ce6396b0527958a9d464d958e9af3
PeppPercoc <paolo.martino1234@gmail.com>
2025-02-07 12:00:34 +0100

    First commit
    Aggiunta cartella creata con spring initializr

---
+++ src/main/java/it/Martino_Gallozzi/Martino_Gallozzi/MartinoGallozziApplication.java
@@ -0,0 +1,13 @@
+package it.Martino_Gallozzi.Martino_Gallozzi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MartinoGallozziApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MartinoGallozziApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/Martino_Gallozzi/Martino_Gallozzi/MartinoGallozziApplicationTests.java
@@ -0,0 +1,13 @@
+package it.Martino_Gallozzi.Martino_Gallozzi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MartinoGallozziApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
