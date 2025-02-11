f277370fc1e8f711c38604c65efee9ec66e85543
Paolo <paolo.martino1234@gmail.com>
2025-02-11 23:35:35 +0100

    Updated UtenteService added methods

--- src/main/java/it/martino_gallozzi/giornale/service/UtenteService.java
+++ src/main/java/it/martino_gallozzi/giornale/service/UtenteService.java
@@ -1,12 +1,45 @@
 package it.martino_gallozzi.giornale.service;
 
+import it.martino_gallozzi.giornale.entity.Utente;
 import it.martino_gallozzi.giornale.repository.UtenteRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 @AllArgsConstructor
 public class UtenteService {
     private final UtenteRepository utenteRepository;
+
+    //Create
+    public Utente insertUtente(Utente utente) {
+        utenteRepository.findUtenteByEmail(utente.getEmail()).ifPresentOrElse(s -> {
+            System.out.println("User " + s + " already exists");
+        }, () -> {
+            System.out.println("Inserting user " + utente);
+            utenteRepository.insert(utente);
+        });
+        return utente;
+    }
+    //Read
+    public Utente getUtentetById(String utenteId) {
+        Optional<Utente> utente = utenteRepository.findById(utenteId);
+        return utente.orElse(null);
+    }
+
+    public Utente getUtentetByEmail(String utenteEmail) {
+        Optional<Utente> utente = utenteRepository.findUtenteByEmail(utenteEmail);
+        return utente.orElse(null);
+    }
+    //Update
+
+    //Delete
+    public String deleteUtenteById(String utenteId) {
+        if (utenteRepository.existsById(utenteId)) {
+            utenteRepository.deleteById(utenteId);
+            return "User " + utenteId + " deleted";
+        } else return "User id is not present in database";
+    }
 
 }
