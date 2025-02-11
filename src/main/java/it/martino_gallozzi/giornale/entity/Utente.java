f6efad8c0f9c7079ffd931469d0699699dc4f047
marioreste <mariogallozzi@gmail.com>
2025-02-11 15:50:08 +0100

    Change: added attributes to Abbonamento.java

--- src/main/java/it/martino_gallozzi/giornale/entity/Utente.java
+++ src/main/java/it/martino_gallozzi/giornale/entity/Utente.java
@@ -7,7 +7,7 @@ import org.springframework.data.mongodb.core.mapping.Document;
 @Data
 @Document
 
-public class Persona {
+public class Utente {
     @Id
     private String id;
     private String nome;
