0a93f53389bca92c4e0c80712835001521f36075
Paolo <paolo.martino1234@gmail.com>
2025-02-11 18:27:50 +0100

    Update UtenteService added @Service @AllArgsConstructor

--- src/main/java/it/martino_gallozzi/giornale/service/UtenteService.java
+++ src/main/java/it/martino_gallozzi/giornale/service/UtenteService.java
@@ -1,4 +1,12 @@
 package it.martino_gallozzi.giornale.service;
 
+import it.martino_gallozzi.giornale.repository.UtenteRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
 public class UtenteService {
+    private final UtenteRepository utenteRepository;
+
 }
