985a334cf87919074137bc99fc5f017489275063
Paolo <paolo.martino1234@gmail.com>
2025-02-11 17:49:27 +0100

    Updated UtenteRepository added findUtenteByEmailAndPassword

--- src/main/java/it/martino_gallozzi/giornale/repository/UtenteRepository.java
+++ src/main/java/it/martino_gallozzi/giornale/repository/UtenteRepository.java
@@ -3,5 +3,8 @@ package it.martino_gallozzi.giornale.repository;
 import it.martino_gallozzi.giornale.entity.Utente;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.Optional;
+
 public interface UtenteRepository extends MongoRepository<Utente, String> {
+    Optional<Utente> findUtenteByEmailAndPassword(String email, String password);
 }
