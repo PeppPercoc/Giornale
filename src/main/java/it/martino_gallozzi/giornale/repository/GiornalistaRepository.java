58f6154d3af9cf949585d42fff493ef8e770ba1b
Paolo <paolo.martino1234@gmail.com>
2025-02-11 17:51:17 +0100

    Updated GiornalistaRepository added findUtenteByNome

--- src/main/java/it/martino_gallozzi/giornale/repository/GiornalistaRepository.java
+++ src/main/java/it/martino_gallozzi/giornale/repository/GiornalistaRepository.java
@@ -3,5 +3,8 @@ package it.martino_gallozzi.giornale.repository;
 import it.martino_gallozzi.giornale.entity.Giornalista;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.Optional;
+
 public interface GiornalistaRepository extends MongoRepository<Giornalista, String> {
+    Optional<Giornalista> findByNome(String nome);
 }
