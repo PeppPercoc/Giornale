d9907513bb1bdb0c09556151e89cbdb4cbc21d47
marioreste <mariogallozzi@gmail.com>
2025-02-12 09:31:03 +0100

    Removed: Argomento
    Removed Argomento entity, ArgomentoController, ArgomentoRepository and ArgomentoService

--- src/main/java/it/martino_gallozzi/giornale/repository/ArgomentoRepository.java
+++
@@ -1,7 +0,0 @@
-package it.martino_gallozzi.giornale.repository;
-
-import it.martino_gallozzi.giornale.entity.Argomento;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface ArgomentoRepository extends MongoRepository<Argomento, String> {
-}
