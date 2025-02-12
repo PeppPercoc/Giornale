b8c3d995317623f91ae137e979f500af35fdfa31
Paolo <paolo.martino1234@gmail.com>
2025-02-12 02:39:13 +0100

    update ArticoloController update controller

--- src/main/java/it/martino_gallozzi/giornale/controller/ArticoloController.java
+++ src/main/java/it/martino_gallozzi/giornale/controller/ArticoloController.java
@@ -1,13 +1,35 @@
 package it.martino_gallozzi.giornale.controller;
 
+import it.martino_gallozzi.giornale.entity.Articolo;
 import it.martino_gallozzi.giornale.service.ArticoloService;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 @RestController
 @RequestMapping("api/v1/articolo")
 @AllArgsConstructor
 public class ArticoloController {
     private final ArticoloService articoloService;
+
+    @PostMapping("insert-articolo")
+    public Articolo insertArticolo(@RequestBody Articolo articolo) {
+        return articoloService.insertArticolo(articolo);
+    }
+
+    @GetMapping("get-by-id/{articoloTitolo}")
+    public Articolo getArticoloById(@PathVariable String articoloTitolo) {
+
+        Articolo articolo = articoloService.getArticoloByTitolo(articoloTitolo);
+        if(articolo!=null){
+            return articolo;
+        }else return null;
+    }
+
+    @DeleteMapping("delete")
+    public String deleteArticolo(@RequestBody Map<String, String> id) {
+        return articoloService.deleteArticoloById(id.get("id"));
+    }
+
 }
