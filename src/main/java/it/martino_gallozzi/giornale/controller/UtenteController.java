4ed0bd0b54181bc25200246c14620e576d9b431f
Paolo <paolo.martino1234@gmail.com>
2025-02-12 02:13:11 +0100

    Updated UtenteController added methods

--- src/main/java/it/martino_gallozzi/giornale/controller/UtenteController.java
+++ src/main/java/it/martino_gallozzi/giornale/controller/UtenteController.java
@@ -1,13 +1,44 @@
 package it.martino_gallozzi.giornale.controller;
 
+import it.martino_gallozzi.giornale.entity.Utente;
 import it.martino_gallozzi.giornale.service.UtenteService;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 @RestController
 @RequestMapping("api/v1/utente")
 @AllArgsConstructor
 public class UtenteController {
     private final UtenteService utenteService;
+
+    @PostMapping("insert-utente")
+    public Utente insertUtente(@RequestBody Utente utente) {
+        return utenteService.insertUtente(utente);
+    }
+
+    @GetMapping("get-by-id/{utenteId}")
+    public Utente getUtenteById(@PathVariable String utenteId) {
+
+        Utente utente = utenteService.getUtentetById(utenteId);
+        if(utente!=null){
+            return utente;
+        }else return null;
+    }
+
+    @GetMapping("get-by-id/{emailId}")
+    public Utente getUtenteByEmail(@PathVariable String utenteEmail) {
+
+        Utente utente = utenteService.getUtentetByEmail(utenteEmail);
+        if(utente!=null){
+            return utente;
+        }else return null;
+    }
+
+    @DeleteMapping("delete")
+    public String deleteUtente(@RequestBody Map<String, String> id) {
+        return utenteService.deleteUtenteById(id.get("id"));
+    }
+
 }
