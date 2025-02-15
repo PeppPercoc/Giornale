9f44fc355aa5bcdb7f2194ff9cc03e6d30843759
Paolo <paolo.martino1234@gmail.com>
2025-02-15 14:08:12 +0100

    removed password in utente

--- src/main/java/it/martino_gallozzi/giornale/entity/Utente.java
+++ src/main/java/it/martino_gallozzi/giornale/entity/Utente.java
@@ -18,10 +18,9 @@ public class Utente {
     private String cap;
     @Indexed(unique = true)
     private String email;
-    private String password;
 
     public Utente(String id, String nome, String cognome, String indirizzo,
-                  String citta, String cap, String email, String password) {
+                  String citta, String cap, String email) {
         this.id = id;
         this.nome = nome;
         this.cognome = cognome;
@@ -29,6 +28,5 @@ public class Utente {
         this.citta = citta;
         this.cap = cap;
         this.email = email;
-        this.password = password;
     }
 }
