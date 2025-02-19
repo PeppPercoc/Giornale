6b50b5c06a4f64eb5cd1d9887efee56769ba6bc6
marioreste <mariogallozzi@gmail.com>
2025-02-19 11:34:48 +0100

    Minor changes in PubblicazioneService

--- src/main/java/it/martino_gallozzi/giornale/service/PubblicazioneService.java
+++ src/main/java/it/martino_gallozzi/giornale/service/PubblicazioneService.java
@@ -86,5 +86,3 @@ public class PubblicazioneService {
     //todo: controllare che quando inserisco una pubblicazione tutti gli id di articolo esistano
     //todo: read the list of the Articoli inside the Pubblicazione
 }
-
-//git commit -a -m "Modified insertPubblicazione in PubblicazionService" -m "Now the method check if pubblicazione.listaUtentiId is empty, and if it's not, it clear the list. The user list must be empty when the publication is created"
