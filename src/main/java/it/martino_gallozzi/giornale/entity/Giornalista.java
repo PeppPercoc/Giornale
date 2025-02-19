65e95a97057bffe512b50c4962309ce063d04106
marioreste <mariogallozzi@gmail.com>
2025-02-19 12:01:28 +0100

    Changed constructor in Giornalista entity

--- src/main/java/it/martino_gallozzi/giornale/entity/Giornalista.java
+++ src/main/java/it/martino_gallozzi/giornale/entity/Giornalista.java
@@ -1,21 +1,17 @@
 package it.martino_gallozzi.giornale.entity;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Data
 @Document
+@AllArgsConstructor
 
 public class Giornalista {
     @Id
     private String id;
     private String nome;
     private String annoIscrizioneAlbo;
-
-    public Giornalista(String id, String nome, String annoIscrizioneAlbo) {
-        this.id = id;
-        this.nome = nome;
-        this.annoIscrizioneAlbo = annoIscrizioneAlbo;
-    }
 }
