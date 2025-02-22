1b7bf32c2d6cff93e8ea355c99edc1a85496a9fd
marioreste <mariogallozzi@gmail.com>
2025-02-22 11:04:20 +0100

    Added 'findPubblicazioneById' in PubblicazioneRepository

--- src/main/java/it/martino_gallozzi/giornale/repository/PubblicazioneRepository.java
+++ src/main/java/it/martino_gallozzi/giornale/repository/PubblicazioneRepository.java
@@ -9,4 +9,6 @@ import java.util.Optional;
 @Repository
 public interface PubblicazioneRepository extends MongoRepository<Pubblicazione, String> {
     Optional<Pubblicazione> findPubblicazioneByTitolo(String titolo);
+
+    Optional<Pubblicazione> findPubblicazioneById(String id);
 }
